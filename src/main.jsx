845f5be545d5447c3bac4c7597b96b49ee64b5b9
hamzafx5 <67003766+hamzafx5@users.noreply.github.com>
2022-06-05 23:54:26 +0100

    fix some bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
 import ReactDOM from "react-dom/client";
-// import ReactDOM from "react-dom";
 import App from "./App";
-import Framer from "./Framer";
 import "./index.css";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(<Framer />);
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
